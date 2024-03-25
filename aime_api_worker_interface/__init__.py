6bfb963d365ac6c155d1280da32403c347ec3f5a
carlo <carlo@aime.info>
2024-03-25 18:33:52 +0100

    Added header with license and copyright to all files

--- aime_api_worker_interface/__init__.py
+++ aime_api_worker_interface/__init__.py
@@ -1,1 +1,5 @@
+# Copyright (c) AIME GmbH and affiliates. Find more info at https://www.aime.info/api
+#
+# This software may be used and distributed according to the terms of the MIT LICENSE
+
 from .api_worker_interface import APIWorkerInterface, DEFAULT_IMAGE_METADATA
