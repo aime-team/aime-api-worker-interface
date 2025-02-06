0273f70122136cbf620dfbe632249aeab3ed22f8
carlo_vogel <carlo@aime.info>
2025-02-06 15:25:28 +0100

    Added download_weights.py with shell command "awi download-weights" to download model weights

--- README.md
+++ README.md
@@ -9,6 +9,14 @@ Run
 pip install git+https://github.com/aime-team/aime-api-worker-interface
 ```
 
+# Download model weights
+
+Example to download Llama-3.3-70B-Instruct:
+
+```
+awi download-weights meta-llama/Llama-3.3-70B-Instruct
+```
+
 # More info
 
 For more info about AIME API Server, please visit: [https://api.aime.info](https://api.aime.info)
--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup
 
-setup(name='aime-api-worker-interface',
-      version='0.9.4',
-      author='AIME',
-      author_email='carlo@aime.info',
-      packages=['aime_api_worker_interface'],
-      zip_safe=False)
+setup(
+    name='aime-api-worker-interface',
+    version='0.9.5',
+    author='AIME',
+    author_email='carlo@aime.info',
+    scripts=['aime_api_worker_interface/awi'],
+    install_requires=[
+        'pillow==10.2.0',
+        'requests==2.31.0',
+        'huggingface_hub==0.28.0',
+    ],
+    packages=['aime_api_worker_interface'],
+    zip_safe=False
+)
