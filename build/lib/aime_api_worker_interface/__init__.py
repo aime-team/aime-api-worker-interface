0e8244e46be79d7add7857934db59b9b173a80ee
toine <toine@aime.info>
2025-04-18 00:13:00 +0200

    removed build folder

--- build/lib/aime_api_worker_interface/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) AIME GmbH and affiliates. Find more info at https://www.aime.info/api
-#
-# This software may be used and distributed according to the terms of the MIT LICENSE
-
-from .api_worker_interface import APIWorkerInterface, DEFAULT_IMAGE_METADATA
